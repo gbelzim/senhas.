c6cef3456fd44309e3ab0ee9973bc4334faec9bf
Gabriel H <gabriel.mendes.oliveira01@escola.pr.gov.br>
2024-10-01 20:44:39 -0300

    m

--- js/perguntas.js
+++ js/perguntas.js
@@ -1,106 +1,172 @@
 export const perguntas = [
     {
-        enunciado: "Assim que você acorda, recebe uma ligação para um trabalho arriscado: roubar um chip de Don Dalpd0z, um ciborgue rico e perigoso. Ao chegar ao local, você avista ele. Qual sua primeira escolha?",
+        enunciado: "Assim que voce acorda, recebe uma ligação para um trabalho arriscado, roubar um chip do Don dalpd0z um ciborgue rico, ao chegar no local do roubo voce avista ele. Qual sua primeira escolha?",
         alternativas: [
             {
-                texto: "Sacar uma arma e apontar para ele!",
+                texto: "saca uma arma e aponta para ele!",
                 afirmacao: [
-                    "Você sentiu o medo tomando conta e decidiu agir rapidamente, sacando sua arma para intimidá-lo.",
-                    "O pensamento de enfrentar uma máquina tão poderosa o fez agir de forma impulsiva."
+                    "No início ficou com medo do que ele podia fazer, e se desesperou e pegou uma arma.",
+                    "Achou assustador o fato que ele podia morrer sem nem conseguir algo."
                 ],
                 proxima: 1,
             },
             {
-                texto: "Esperar o momento certo para atacar!",
+                texto: "esperou o momento certo para atacar ele!",
                 afirmacao: [
-                    "Você manteve a calma e analisou a situação, esperando o momento perfeito para atacar.",
-                    "Sua paciência pode ser a chave para pegar Don Dalpd0z de surpresa."
+                    "Quis saber como usar o cenário que ele estava para se salvar.",
+                    "Pensou como se disfarçar na mutidão para emboscar ele."
                 ],
                 proxima: 2,
             },
         ]
     },
     {
-        enunciado: "Agora que você está frente a frente com o ciborgue, ele parece mais perigoso do que você imaginava. 100kg de pura tecnologia avançada. O que você faz?",
+        enunciado: "Ficar frente a frente com ele é bem amedrontador, 100kg de pura tecnologia em um corpo de um homem como é possivel?. Qual atitude você toma?",
         alternativas: [
             {
-                texto: "Atacar diretamente com uma arma!",
+                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
                 afirmacao: [
-                    "Você decidiu partir para o confronto, sacando sua arma e atirando antes que ele possa reagir.",
-                    "A decisão foi ousada, mas perigosa. Agora, é tudo ou nada!"
+                    "Conseguiu utilizar a IA para buscar informações úteis.",
+                    "Percebeu que a IA pode ajudar a encontrar informações úteis na internet de forma mais rápida e direcionada.",
+                    "Percebeu que a IA consegue explicar termos complicados de forma simplificada e isso ajudou muito suas pesquisas sobre assuntos complexos."
                 ],
                 proxima: 3,
             },
             {
-                texto: "Tentar hackear o sistema dele à distância!",
+                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
                 afirmacao: [
-                    "Optou por uma abordagem mais estratégica, tentando acessar o sistema do ciborgue e desativá-lo antes que ele perceba.",
-                    "Se o hack for bem-sucedido, você poderá evitar um confronto direto."
+                    "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho.",
+                    "Achou que era muito mais fácil procurar por respostas utilizando alguns meios mais tradicionais mesmo que levasse mais tempo.",
+                    "Sentiu um pouco de medo de quais dados pessoais seus a IA poderia utilizar e por isso prefere fazer suas coisas com pouca intromissão da tecnologia."
                 ],
                 proxima: 4,
             },
         ]
     },
     {
-        enunciado: "Durante o confronto, você percebe que o chip está integrado ao cérebro dele. Como você pretende extrair o chip?",
+        enunciado: "Um chat com IA pode ser uma boa alternativa para realizar tarefas do dia a dia e foi pensando nisso que uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
         alternativas: [
             {
-                texto: "Desativar o ciborgue e fazer a extração com cuidado!",
+                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
                 afirmacao: [
-                    "Sua estratégia é desativá-lo temporariamente e fazer a extração sem causar danos irreparáveis.",
-                    "É uma abordagem mais técnica, que exige precisão."
+                    "Conseguiu utilizar a IA para buscar informações úteis.",
+                    "Percebeu que a IA pode ajudar a encontrar informações úteis na internet de forma mais rápida e direcionada.",
+                    "Percebeu que a IA consegue explicar termos complicados de forma simplificada e isso ajudou muito suas pesquisas sobre assuntos complexos."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+                afirmacao: [
+                    "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho.",
+                    "Achou que era muito mais fácil procurar por respostas utilizando alguns meios mais tradicionais mesmo que levasse mais tempo.",
+                    "Sentiu um pouco de medo de quais dados pessoais seus a IA poderia utilizar e por isso prefere fazer suas coisas com pouca intromissão da tecnologia."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Ferramentas de busca que utilizam IA podem facilitar muito nosso processo de busca por informações. Agora imagine que você precisa participar de um debate sobre o quanto a IA impacta no trabalho do futuro. Qual posicionamento tomar?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
                 ],
                 proxima: 5,
             },
             {
-                texto: "Arriscar uma extração rápida durante o combate!",
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
                 afirmacao: [
-                    "Você decidiu correr o risco e tentar extrair o chip enquanto luta com ele.",
-                    "Embora seja uma jogada arriscada, pode poupar tempo e impedir que ele reaja."
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
                 ],
                 proxima: 6,
             },
         ]
     },
     {
-        enunciado: "Você conseguiu o chip, mas os alarmes da instalação começam a soar. Como você planeja escapar?",
+        enunciado: "Utilizar informações próprias traz um toque pessoal ao nosso trabalho. Agora imagine que você precisa participar de um debate sobre o quanto a IA impacta no trabalho do futuro. Qual posicionamento tomar?",
         alternativas: [
             {
-                texto: "Usar uma rota de fuga pré-planejada!",
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
                 afirmacao: [
-                    "Como parte do seu plano, você já tinha mapeado a instalação e sabia exatamente para onde ir.",
-                    "Sua preparação pode ser o diferencial entre a vida e a morte."
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
                 ],
-                proxima: 7,
+                proxima: 5,
             },
             {
-                texto: "Roubar um veículo e escapar pelos ares!",
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
                 afirmacao: [
-                    "Você decidiu improvisar, roubando um dos veículos de segurança da instalação e tentando escapar voando.",
-                    "Um movimento ousado, mas pode ser o que vai garantir sua fuga."
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
                 ],
-                proxima: 8,
+                proxima: 6,
             },
         ]
     },
     {
-        enunciado: "Você está quase fora da instalação, mas os drones de segurança estão atrás de você. Como você lida com eles?",
+        enunciado: "Novas tecnologias trazem novos desafios, por isso após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
         alternativas: [
             {
-                texto: "Disparar contra os drones!",
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
                 afirmacao: [
-                    "Você decidiu enfrentar os drones com fogo pesado, tentando derrubá-los antes que eles o alcancem.",
-                    "É um confronto direto, mas pode ser necessário para garantir sua sobrevivência."
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
                 ],
-                proxima: 9,
+                proxima: 7,
             },
             {
-                texto: "Usar dispositivos de hack para desativá-los!",
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
                 afirmacao: [
-                    "Você usou seus dispositivos de hack para desativar os drones remotamente.",
-                    "Uma abordagem mais técnica que pode ser mais segura do que bater de frente com ele."
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
                 ],
-                proxima: 10,
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "É muito importante refletir sobre os trabalhadores do futuro. Por isso a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        alternativas: [
+            {
+                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+                afirmacao: [
+                    "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes.",
+                    "Ainda acha que os meios de desenho tradicionais são mais eficazes para a criatividade, por isso vem estimulando pessoas em suas redes sociais a fazer pintura em aquarela."
+                ],
+            },
+            {
+                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+                afirmacao: [
+                    "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!",
+                    "Compartilhou artes em redes sociais como forma de ensinar como se comunicar através da arte.",
+                    "Percebeu que muitas pessoas têm dificuldade em expressar suas ideias desenhando e acha que a IA é capaz de empoderar essas pessoas a tirarem ideias do papel."
+                ],
             },
         ]
     }
